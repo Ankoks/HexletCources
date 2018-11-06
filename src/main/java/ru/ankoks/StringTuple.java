09cfec5967ea1182f46a80c479d23b72b6b7e810
ankoks <koksharov.work@gmail.com>
2018-11-06 16:51:54 +0300

    add StringTuple dto for concrete class

---
+++ src/main/java/ru/ankoks/StringTuple.java
@@ -0,0 +1,24 @@
+package ru.ankoks;
+
+/**
+ * User: ankoks
+ * Date: 06.11.2018
+ */
+public class StringTuple {
+    private final String left;
+
+    private final String right;
+
+    public StringTuple(String left, String right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public String getLeft() {
+        return left;
+    }
+
+    public String getRight() {
+        return right;
+    }
+}
