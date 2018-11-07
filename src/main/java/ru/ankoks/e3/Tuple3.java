685db8553c183bf005cec466ff6e6c007dc75bfb
ankoks <koksharov.work@gmail.com>
2018-11-07 18:20:10 +0300

    extend class

--- src/main/java/ru/ankoks/e3/Tuple3.java
+++ src/main/java/ru/ankoks/e3/Tuple3.java
@@ -7,9 +7,10 @@ package ru.ankoks.e3;
 public class Tuple3<T1, T2> {
 
     private final T1 left;
+
     private final T2 right;
 
-    public Tuple3(T1 left, T2 right) {
+    public Tuple3(final T1 left, final T2 right) {
         this.left = left;
         this.right = right;
     }
