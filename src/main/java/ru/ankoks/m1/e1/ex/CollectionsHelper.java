a5281fadcef82b43f6f4ea1a0c6bf1f5a9c27b31
ankoks <koksharov.work@gmail.com>
2018-11-08 14:40:20 +0300

    m2.e1. exercise

---
+++ src/main/java/ru/ankoks/m1/e1/ex/CollectionsHelper.java
@@ -0,0 +1,41 @@
+package ru.ankoks.m1.e1.ex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: ankoks
+ * Date: 08.11.2018
+ */
+public class CollectionsHelper<T extends Human> {
+
+
+    public T findFirst(final List<T> humans, final String namePrefix, final Sex sex) {
+        // BEGIN (write your solution here)
+        for (int i = 0; i < humans.size(); i++) {
+            if (humans.get(i).getName().startsWith(namePrefix) && humans.get(i).getSex().equals(sex)) {
+                return humans.get(i);
+            }
+        }
+
+        return null;
+        // END
+    }
+
+    public static void main(String[] args) {
+        List<Human> mans = new ArrayList<Human>(){
+            {
+                add(new Man("Slava"));
+                add(new Man("Klod"));
+                add(new Woman("Klara"));
+                add(new Woman("Tamara"));
+                add(new Intersex("Borodachka"));
+            }
+        };
+
+
+        CollectionsHelper<Human> collectionsHelper = new CollectionsHelper<>();
+
+        System.out.println(collectionsHelper.findFirst(mans, "Kl", Sex.F).getName());
+    }
+}
---
+++ src/main/java/ru/ankoks/m1/e1/ex/Woman.java
@@ -0,0 +1,23 @@
+package ru.ankoks.m1.e1.ex;
+
+/**
+ * User: ankoks
+ * Date: 08.11.2018
+ */
+public class Woman implements Human {
+    private final String name;
+
+    public Woman(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Sex getSex() {
+        return Sex.F;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
