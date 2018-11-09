ede32cf3964f4280308101599d1fcfb0526ceb64
ankoks <koksharov.work@gmail.com>
2018-11-09 16:05:10 +0300

    m2.e0. exercise

---
+++ src/main/java/ru/ankoks/m2/e0/ex/CopyController.java
@@ -0,0 +1,12 @@
+package ru.ankoks.m2.e0.ex;
+
+/**
+ * User: ankoks
+ * Date: 09.11.2018
+ */
+public class CopyController {
+    public static <T extends IPrinter & IScanner> void copy(T device) {
+        String scan = device.scan();
+        device.print(scan);
+    }
+}
