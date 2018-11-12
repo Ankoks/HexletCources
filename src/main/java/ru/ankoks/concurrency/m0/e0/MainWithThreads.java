90a8cad4043570a13ae91887412d9b31691fb2dc
ankoks <koksharov.work@gmail.com>
2018-11-12 12:27:28 +0300

    concurrency. m0.e0. concurrent code

---
+++ src/main/java/ru/ankoks/concurrency/m0/e0/MainWithThreads.java
@@ -0,0 +1,27 @@
+package ru.ankoks.concurrency.m0.e0;
+
+/**
+ * User: ankoks
+ * Date: 12.11.2018
+ */
+public class MainWithThreads {
+    public static void main(String[] args) {
+        final ITextLoader fileTextLoader = new MockLoader("textFromFile");
+        final ITextLoader webTextLoader = new MockLoader("textFromWeb");
+        final long before = System.currentTimeMillis();
+
+        final Thread fileTextLoaderThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(fileTextLoader.loadText());
+            }
+        });
+        fileTextLoaderThread.start();
+        final Thread webTextLoaderThread = new Thread(() -> System.out.println(webTextLoader.loadText()));
+        webTextLoaderThread.start();
+
+        final long after = System.currentTimeMillis();
+
+        System.out.printf("Time delta: %d", (after-before)/1_000);
+    }
+}
