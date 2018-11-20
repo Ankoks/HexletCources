fd9bce10729608a00ee32077b4fe388c655544ea
ankoks <koksharov.work@gmail.com>
2018-11-20 12:16:40 +0300

    concurrency.m3.e0. init

---
+++ src/main/java/ru/ankoks/concurrency/m3/e0/IntList.java
@@ -0,0 +1,28 @@
+package ru.ankoks.concurrency.m3.e0;
+
+/**
+ * User: ankoks
+ * Date: 20.11.2018
+ */
+public class IntList {
+    private int[] data = new int[10];
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void add(int element) {
+        if (data.length == size) {
+            int[] newData = new int[data.length * 2 + 1];
+            System.arraycopy(data, 0, newData, 0, data.length);
+            data = newData;
+        }
+
+        data[size++] = element;
+    }
+
+    public int getElement(int index) {
+        return data[index];
+    }
+}
---
+++ src/main/java/ru/ankoks/concurrency/m3/e0/Main.java
@@ -0,0 +1,46 @@
+package ru.ankoks.concurrency.m3.e0;
+
+/**
+ * User: ankoks
+ * Date: 20.11.2018
+ */
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        final IntList intList = new IntList();
+        final Thread t1 = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10_000; i++) {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    intList.add(i);
+                }
+            }
+        };
+
+        final Thread t2 = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10_000; i++) {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    intList.add(i);
+                }
+            }
+        };
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.printf("Size: %d", intList.getSize());
+    }
+}
